7c469b40ce68372b1ac0b87171026dc4754d39cb
Brett Viren <brett.viren@gmail.com>
2015-01-24 10:52:35 -0500

    Some forgotten commits.

---
+++ python/orgmode/dumper/xml.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+'''
+Dump an orgmode.element.Node into XML following an unstated schema that mimics that of org-element.
+'''
+
+def dump(node):
+    s = 
+    
---
+++ python/orgmode/orgjson.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+'''Support code for using JSON files written by org-json.
+
+This module provides Python objects that attempt to adhere to the
+schema that is followed by org-element as documented at:
+
+http://orgmode.org/worg/exporters/org-element-docstrings.html
+
+This module provides an expression of this schema.
+
+'''
+
+schema = 
+
+import json
+from collections import namedtuple
+
+
